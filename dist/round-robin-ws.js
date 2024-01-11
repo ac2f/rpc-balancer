cad577ed719fabb56a5a4d7567fbcab3502a31a9
ac2f <ww>
2024-01-11 19:29:36 +0300

    .

--- dist/round-robin-ws.js
+++ dist/round-robin-ws.js
@@ -105,8 +105,8 @@ class RoundRobinWS {
             const _subscription = await provider.subscribe(subscription);
             subscriptionIds.push(_subscription.id);
             _subscription.on("data", (data) => {
-                console.log(this.subscriptionResults[data.transactionHash]);
                 if (!this.subscriptionResults[data.transactionHash]) {
+                    console.log("hash not exists", data.transactionHash, this.subscriptionResults[data.transactionHash]);
                     this.subscriptionResults[data.transactionHash] = data;
                     this.emit("data", data);
                 }
