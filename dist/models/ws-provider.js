fcf0bd26bb697b24d1a351c12f844ffeade3e5be
ac2f <ww>
2024-01-12 23:34:10 +0300

    .

--- dist/models/ws-provider.js
+++ dist/models/ws-provider.js
@@ -66,7 +66,6 @@ class WSProvider extends web3_1.WebSocketProvider {
                 const subscriptionId = message.params.subscription;
                 const subscription = this.getSubscriptionById(subscriptionId);
                 if (subscription) {
-                    console.log("emitting", subscription);
                     subscription.emit("data", message.params.result);
                 }
                 else {
--- src/models/ws-provider.ts
+++ src/models/ws-provider.ts
@@ -65,7 +65,6 @@ export class WSProvider extends WebSocketProvider implements IWSProvider {
                 const subscriptionId: string = message.params.subscription;
                 const subscription = this.getSubscriptionById(subscriptionId);
                 if (subscription) {
-                    console.log("emitting", subscription);
                     subscription.emit("data", message.params.result);
                 } else {
                     console.log("no subscription", this.subscriptionAliasToId, this.subscriptionIdToAlias, message);
